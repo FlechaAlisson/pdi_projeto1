8da2f04ccb5b6826131b81d5863cd45aae1d727d
Guilherme Sotoa <potaoew@gmail.com>
2020-10-13 22:35:03 -0300

    não testei muito.

--- src/Main.java
+++ src/Main.java
@@ -54,54 +54,60 @@ public class Main {
 
         Main m = new Main();
         ArrayList<Integer> i = new ArrayList();
-        i.add(5234);
+        i.add(1234);
         i.add(5678);
-        i.add(9);
+        i.add(321);
+        i.add(543);
+
         System.out.println(i);
 
-        //m.atualizaCode(i);
+        m.atualizaCode(i);
         System.out.println(i);
     }
 
-//    void atualizaCode(ArrayList<Integer> arrayList){
-//        int numero = arrayList.get(0);
-//        int ultimoDigito = numero;
-//        int qt =0;
-//
-//        /**
-//        Pega o ultimo número do primeiro digito
-//         */
-//        while (ultimoDigito >= 10) {
-//            ultimoDigito /= 10;
-//            qt++;
-//        }
-//
-//        /**
-//        * Atualiza o primeiro numero
-//        * */
-//        arrayList.set(0, (int) ((numero) - (pow(10,qt)) * ultimoDigito));
-//
-//        numero = arrayList.get(1);
-//        ultimoDigito = numero;
-//        qt =0;
-//
-//        for (int i = 2; i <= arrayList.size()-1; i++) {
-//            while (ultimoDigito >= 10) {
-//                ultimoDigito /= 10;
-//                qt++;
-//            }
-//
-//            /*
-//             * Atualiza o valor e puxa
-//             */
-//
-//            numero = arrayList.get(i);
-//            ultimoDigito = numero;
-//            System.out.println(numero);
-////            qt =0;
-//        }
-//
-//    }
+    void atualizaCode(ArrayList<Integer> arrayList){
+        int i, num_decimal_atual, num_decimal_prox, encaixe, primeiro_atual;
+        int n_digitos_atual;
+        int n_digitos_proximo;
+
+
+        for(i = 0; i <= arrayList.size()-1 ; i++){
+
+            n_digitos_atual = 0;
+            n_digitos_proximo = 0;
+            num_decimal_atual = num_decimal_prox = 1;
+
+            //pega o numero de digitos indice atual
+            for(int aux = arrayList.get(i); aux != 0 ; aux/=10, n_digitos_atual++);
+            //pega o numeral decimal
+            while (n_digitos_atual > 1){
+                num_decimal_atual *= 10;
+                n_digitos_atual--;
+            }
+            //pega o primeiro digito atual
+            primeiro_atual = (arrayList.get(i) / num_decimal_atual);
+
+            //modifica o indice atual
+            arrayList.set(i, arrayList.get(i) - num_decimal_atual*primeiro_atual);
+
+            //apenas registra o encaixe caso o indice atual seja menor que o ultimo indice
+            if(i < arrayList.size()-1) {
+
+                //pega o numero de digitos do proximo indice
+                for (int aux = arrayList.get(i + 1); aux != 0; aux /= 10, n_digitos_proximo++) ;
+                while (n_digitos_proximo > 1) {
+                    num_decimal_prox *= 10;
+                    n_digitos_proximo--;
+                }
+                //pega o primeiro do proximo
+                encaixe = (arrayList.get(i + 1) / num_decimal_prox);
+                arrayList.set(i, (arrayList.get(i) * 10) + encaixe);
+            }
+            //System.out.println(arrayList.get(i));
+        }
+
+
+    }
 
 
 }
